5de53c613e93410160569f8a2cd1e6c6204129f3
jason <jasonedlewis@gmail.com>
2019-09-10 17:45:47 -0400

    Its halftime

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :meals
+    has_many :meals, dependent: :destroy
     validates :name, :age, :weight, presence: true
     validates :age, inclusion: {in: 10..100}
     validates :weight, inclusion: {in: 75..500}
